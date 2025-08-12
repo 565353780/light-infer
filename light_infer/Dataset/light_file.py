c09514b54caa7e2a24dc98de8cf008e67b9c8f48
chLi <565353780@qq.com>
2025-08-12 11:42:00 +0800

    fix data_list name bug

--- light_infer/Dataset/light_file.py
+++ light_infer/Dataset/light_file.py
@@ -19,21 +19,21 @@ class LightFileDataset(Dataset):
 
         assert isinstance(light_data_dict, dict)
 
-        self.xy_data = torch.from_numpy(light_data_dict["XYDATA"])
+        self.data_list = torch.from_numpy(light_data_dict["XYDATA"])
         return
 
     def __len__(self):
-        return len(self.xy_data)
+        return len(self.data_list)
 
     def __getitem__(self, index):
-        index = index % len(self.xy_data)
+        index = index % len(self.data_list)
 
         if self.split == "train":
             np.random.seed()
         else:
             np.random.seed(1234)
 
-        xy_data = self.xy_data[index]
+        xy_data = self.data_list[index]
 
         data = {
             "input": xy_data[0].to(self.dtype),
